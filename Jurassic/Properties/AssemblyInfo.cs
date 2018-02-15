ab06b070792c11ceb8bbe324fc97c27b80ae4c8a
Michael Kriese <michael.kriese@visualon.de>
2018-02-15 07:31:14 +0100

    Added signing.

--- Jurassic/Properties/AssemblyInfo.cs
+++ Jurassic/Properties/AssemblyInfo.cs
@@ -3,12 +3,8 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 
-// The AllowPartiallyTrustedCallersAttribute requires the assembly to be signed with a strong name
-// key.
-//[assembly: System.Security.AllowPartiallyTrustedCallers]
-
 // Unit tests and performance tests need access to internal members.
-[assembly: InternalsVisibleTo("Unit Tests")]
-//[assembly: InternalsVisibleTo("Performance")]
-//[assembly: InternalsVisibleTo("JavaScript")]
-//[assembly: InternalsVisibleTo("DebuggerTest")]
+[assembly: InternalsVisibleTo("Unit Tests, PublicKey=00240000048000009400000006020000002400005253" +
+    "413100040000010001000d9e3222b4ac5e04d6ea884cd918e62997c023dec31205991d186bb880b34edc455cb14b" +
+    "50c9751b42605c51c36b869df29b9f17ff2b5a44332ba06d52aebd58cf04a1366018cf14907ed9fc6e4ab979a791" +
+    "e283dfe1b5f55b1b3fb13bfa9456d7cfda968f442cadeb73586edc7594e3251965eb2ee14ad753355e9207d89ba4")]
